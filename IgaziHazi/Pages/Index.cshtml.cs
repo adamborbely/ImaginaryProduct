2eabeca8fc9934d14c348a1ed0ab622ae34e784b
Adam <borbelya90@gmail.com>
2020-05-21 12:14:38 +0200

    here

---
+++ IgaziHazi/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace IgaziHazi.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+
+            if (Request.Form["email"].Equals("pl@mosodio.hu"))
+            {
+                Response.Redirect("https://www.index.hu");
+
+            }
+        }
+    }
+}
